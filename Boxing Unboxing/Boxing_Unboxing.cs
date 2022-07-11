c948ad19ff959916b5914355324e9455b4baddf5
SwapnilPadave <Spadave7@gmail.com>
2022-07-11 11:51:47 +0530

    Boxing_Unboxing

---
+++ Boxing Unboxing/Boxing_Unboxing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Boxing_Unboxing
+{
+    public class Boxing_Unboxing
+    {
+        static void Main()
+        {
+            //int i = 1;
+            //object o = i; // boxing  
+            //int j = (int)o; // unboxing  
+            //Console.WriteLine(o);
+            int i = 12;
+            object box = i;
+            if (box is int)
+            {
+                Console.Write("Box contains an int ");
+            }
+            Console.WriteLine(box);
+        }
+    }
+}
--- Enum (Enumeration)/Program.cs
+++ Enum (Enumeration)/Program.cs
@@ -2,18 +2,40 @@
 
 namespace Enum__Enumeration_
 {
-    public class Program
+    //public class Program
+    //{
+    //    public enum Season
+    //    {
+    //        Winter, Spring, Summer, Fall
+    //    };
+    //    public static void Main(string[] args)
+    //    {
+    //        int x = (int)Season.Winter;
+    //        int y = (int)Season.Fall;
+    //        Console.WriteLine("Winter={0}", x);
+    //        Console.WriteLine("Fall={0}", y);
+    //    }
+    //}
+    class Program
     {
-        public enum Season
+        public enum DayofWeek
         {
-            Winter, Spring, Summer, Fall
-        };
-        public static void Main(string[] args)
+            Sunday = 1, Monday, Tuesday = 1, Wednesday, Thursday = 2, Friday, Saturday
+        }
+        static void Main(string[] args)
         {
-            int x = (int)Season.Winter;
-            int y = (int)Season.Fall;
-            Console.WriteLine("Winter={0}", x);
-            Console.WriteLine("Fall={0}", y);
+            string[] values = Enum.GetNames(typeof(DayofWeek));
+            foreach (string s in values)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            int[] n = (int[])Enum.GetValues(typeof(DayofWeek));
+            foreach (int x in n)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
         }
     }
 }
